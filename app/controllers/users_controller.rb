46d28782676aec0894381a178e766586023f51b3
simple <simple.continue@gmail.com>
2018-02-26 20:28:09 +0800

    fix bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user
+  before_action :authenticate_user, except: [:server_config]
 
   def upload_res_token
     @box = Box.find(params[:box_id])
